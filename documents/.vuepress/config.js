a43415a7c29b85f51738962cef5bb770869b984f
aupous <tuan.phamvan.m@gmail.com>
2019-09-27 15:38:51 +0700

    feat: a good first program

--- documents/.vuepress/config.js
+++ documents/.vuepress/config.js
@@ -1,5 +1,21 @@
 module.exports = {
   title: 'Pythong',
   description: 'Thong Thong with Py',
-  base: '/pythong/'
+  base: '/pythong/',
+  themeConfig: {
+    displayAllHeaders: true,
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' }
+    ],
+    sidebar: [
+      {
+        title: 'Guide',
+        children: [ 
+          ['/guide/','Setup'],
+          ['/guide/a_good_first_program' , 'A Good First Program' ]
+        ]
+      }
+    ]
+  }
 }
---
+++ documents/guide/index.md
@@ -0,0 +1,11 @@
+# Setup
+
+Trong phần này, bạn sẽ không code mà chỉ hoàn thành cài đặt máy tính đề có thể chạy Python.
+
+## Mac OSX
+
+Các máy Mac đều đã được cài sẵn Python 2, cần cài thêm Python3 bằng Homebrew.
+
+```
+brew install python3
+```
