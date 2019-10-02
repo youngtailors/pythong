d97ff3bf8913932df75602da5b3f3ab4a09bb076
aupous <tuan.phamvan.m@gmail.com>
2019-10-02 11:34:28 +0700

    fix: fix configjs

--- documents/.vuepress/config.js
+++ documents/.vuepress/config.js
@@ -19,10 +19,10 @@ module.exports = {
           ['/guide/ex4_variables_and_names' , 'Biến và đặt tên' ],
           ['/guide/ex5_more_variables_and_printing' , 'Thêm về biến và đặt tên' ],
           ['/guide/ex6_strings_and_text' , 'String và văn bản' ],
-          ['/guide/ex7_more_printing' , 'Thêm về printing'],,
-          ['/guide/ex8_printing_printing' , 'Thêm về printing (2)'],,
-          ['/guide/ex9_printing_printing_printing' , 'Thêm về printing (3)'],,
-          ['/guide/ex10_what_was_that' , 'Thêm về printing (4)'],
+          ['/guide/ex7_more_printing' , 'Thêm về printing'],
+          ['/guide/ex8_printing_printing' , 'Thêm về printing (2)'],
+          ['/guide/ex9_printing_printing_printing' , 'Thêm về printing (3)'],
+          ['/guide/ex10_what_was_that' , 'Thêm về printing (4)']
         ]
       }
     ]
